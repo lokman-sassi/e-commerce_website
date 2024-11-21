70ebfac3db435eea0ba23036f1d70ae9ac263844
SASSI Lokman <lokman.sassi@univ-constantine2.dz>
2024-11-21 16:11:25 +0100

    Added Espace Membre

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/connexion', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        //Gérer les erreurs
+        $error = $authenticationUtils -> getLastAuthenticationError();
+
+        //Dernier username (email)
+        $lastUserName = $authenticationUtils -> getLastUsername();
+
+        return $this->render('login/index.html.twig', [
+            'error' => $error,
+            'last_username' => $lastUserName,
+        ]);
+    }
+
+    #[Route('/deconnexion', name: 'app_logout', methods: ['GET'])]
+    public function logout(): Response
+    {
+        throw new \Exception('Don\'t forget to activae logout in security.yaml');
+    }
+}
